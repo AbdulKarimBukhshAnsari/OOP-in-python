3b1fdc48fda24cd64bbbc04fda92482d91a8379b
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-04-14 10:58:15 +0500

    basic command to make objects and classes

---
+++ Classes and objects/objects_and_classes.py
@@ -0,0 +1,18 @@
+#you can define the class like that 
+
+class Student:
+    name = "Karim"
+
+#you can define the object like that 
+s_1 = Student()
+
+#to use the attribute of the class 
+print(s_1.name)
+
+"""the best thing of OOP is that you can use the attributes of the class several time
+depends upon how many times you have made the object """
+
+s_2 = Student()
+
+print(s_2.name)
+
