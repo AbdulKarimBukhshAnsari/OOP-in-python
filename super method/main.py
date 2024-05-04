29d7392cc852743877c271b82cc81fc00ce95070
AbdulKarimBukhshAnsari <hiddenmystery621@gmail.com>
2024-05-04 19:11:03 +0500

    Updtaed the super method

--- super method/main.py
+++ super method/main.py
@@ -4,9 +4,9 @@ class Person:
         self.name = name
 class Doctor(Person):
     def __init__(self, salary,name):
-        super().__init__(name) #this is how can we inheir the value of 
+        super().__init__(name) #this is how can we inherit  the value of the parent constructor 
         self.salary = salary
  
 s1 = Doctor(15000,"yasir")
-print(s1.name)
+print(s1.name)  #accessing the value of parent class
 print(s1.salary)
